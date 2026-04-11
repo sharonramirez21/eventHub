a7ca0f9788285f4e4c7a90c645648436072584fe
sharonramirez21 <sharonramirez421@gmail.com>
2026-04-11 11:48:15 -0300

    message in authorization is changed

--- src/middleware/authorizationMiddleware.js
+++ src/middleware/authorizationMiddleware.js
@@ -2,7 +2,7 @@ const isAdmin = (req, res, next) => {
     if (req.user && req.user.role === "admin") {
         return next();
     }
-    return res.status(403).json({ message: "Forbidden" });
+    return res.status(403).json({ message: "You need to be and Admin" });
 };
 
 module.exports = { isAdmin };
