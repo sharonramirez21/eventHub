b2e8057b053fd1b892ec11e9cf1cc320e6ff5626
sharonramirez21 <sharonramirez421@gmail.com>
2026-04-11 11:30:53 -0300

    errors fixed

--- src/routes/registrationRoutes.js
+++ src/routes/registrationRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const registrationController = require('../controllers/registrationController');
 const router = express.Router();
 const { validateRegistration } = require('../validations/registrationValidation');
-const {isAuthenticated} = require('../middleware/authenticate')
+const { isAuthenticated } = require('../middleware/authenticate')
 
 
 router.get('/', registrationController.getAllRegistrations);
 router.get('/:id', registrationController.getOneRegistration);
-router.post('/', isAuthenticated, validateRegistration ,registrationController.createRegistration);
+router.post('/', isAuthenticated, validateRegistration, registrationController.createRegistration);
 router.put("/:id", isAuthenticated, registrationController.updateRegistration);
-router.delete('/:id',isAuthenticated,  registrationController.deleteRegistration);
+router.delete('/:id', isAuthenticated, registrationController.deleteRegistration);
 
 module.exports = router;
