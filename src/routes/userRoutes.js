a99ca9dbcf3f298ef1088a99a76cc5fb7fefeb13
sharonramirez21 <sharonramirez421@gmail.com>
2026-04-14 12:51:08 -0300

    fixwe error

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { validateUser } = require('../validations/userValidation');
 const { isAdmin } = require("../middleware/authorizationMiddleware");
 
-router.get('/', isAdmin, userController.getAllUsers);
+router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getOneUser);
 router.post('/', validateUser, userController.createUser);
 router.put('/:id', validateUser, userController.updateUser);
